ed0ca755c71077cd3b588bdf77beccd7a10ddef5
rahulhotta <77201683+rahulhotta@users.noreply.github.com>
2025-03-16 20:03:17 +0530

    added drag and drop feature

--- app/Components/fileConverter/fileConverter.tsx
+++ app/Components/fileConverter/fileConverter.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useCallback } from 'react';
 import './fileConverter.scss';
 import Image from 'next/image';
 import littleToolsBanner from '../../../public/Images/littleTools_banner.png';
 import { Col, Row } from 'antd';
+import { useDropzone } from 'react-dropzone';
+
 function FileConverter() {
+    const onDrop = useCallback((acceptedFiles: any) => {
+        // Handle the uploaded files here
+        acceptedFiles.forEach((file: any) => {
+            const reader = new FileReader();
+
+            reader.onload = () => {
+                const fileContent = reader.result;
+                console.log('File content:', fileContent);
+            };
+
+            reader.readAsText(file);
+        });
+    }, []);
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
     return (
         <div>
             <Row>
@@ -11,16 +28,25 @@ function FileConverter() {
                     <Image src={littleToolsBanner} alt='little tools landing page banner' className='fileConverter_banner' />
                 </Col>
             </Row>
-            <Row className='file_converter_file_input_container'>
+            <Row {...getRootProps()} className={`dropzone file_converter_file_input_container ${isDragActive ? 'active' : ''}`}>
+                <input {...getInputProps()} />
                 <Col span={24} className='file_converter_col'>
                     <button className='addFile_button'> + Add File</button>
                 </Col>
-                <Col span={24} className='file_converter_col'>
-                    OR
-                </Col>
-                <Col span={24} className='file_converter_col'>
-                    Drag and drop your file HERE.
-                </Col>
+                {isDragActive ? (
+                    <Col span={24} className='file_converter_col'>
+                        Drop Your File Here.
+                    </Col>
+                    ) : (
+                    <>
+                        <Col span={24} className='file_converter_col'>
+                            OR
+                        </Col>
+                        <Col span={24} className='file_converter_col'>
+                            Drag and drop your file HERE.
+                        </Col>
+                    </>
+                )}
             </Row>
         </div>
     )
