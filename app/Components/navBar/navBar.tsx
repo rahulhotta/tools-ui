959c746cca0db55da185aabb0313e8f2d3dfaba2
rahulKumarHotta <rahulkumar.hotta@rockwellautomation.com>
2025-04-12 18:47:13 +0530

    style changes

--- app/Components/navBar/navBar.tsx
+++ app/Components/navBar/navBar.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import './navBar.scss'
+import './navBar.scss';
+import CommonCard from '@/app/Utils/CommonElements/Card/Card';
 function NavBar() {
   return (
-    <div className='nav_logo_container'>littleTools</div>
+    <div className='nav_bar_container'>
+      <CommonCard>
+        <div className='nav_bar'>
+          littleTools
+        </div>
+      </CommonCard>
+    </div>
   )
 }
 
